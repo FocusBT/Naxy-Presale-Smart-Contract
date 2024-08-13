a93b432e7f739524f7bb4e34a7d5373c0f34d022
FocusBT <waseeabd@gmail.com>
2024-08-13 12:18:00 +0500

    added naxy token

---
+++ src/Naxy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract Naxy is ERC20, ERC20Permit {
+    constructor() ERC20("Naxy", "SPY") ERC20Permit("Naxy") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+}
