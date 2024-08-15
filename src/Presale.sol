c041371d72da93c1035a0550618c24cc9a4354bc
FocusBT <waseeabd@gmail.com>
2024-08-15 11:15:41 +0500

    removed chainlink for testing, and cleaned code for testing

--- src/Presale.sol
+++ src/Presale.sol
@@ -4,18 +4,20 @@ pragma solidity ^0.8.20;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Presale  {
-    AggregatorV3Interface internal dataFeedBNB;
-    AggregatorV3Interface internal dataFeedETH;
+    // AggregatorV3Interface internal dataFeedBNB;
+    // AggregatorV3Interface internal dataFeedETH;
 
     mapping (address => uint) public totalTokensBought;
     mapping (address => uint) public referralIncome;
     
     IERC20 public NAXY;
     address public USDT_ADDRESS;
+    address public BUSDC_ADDRESS;
     address public WETH_ADDRESS;
+
     address public owner;
 
     uint public currentPrice;
@@ -26,16 +28,17 @@ contract Presale  {
     uint public amountInBNB;
     
     constructor() {
-        dataFeedBNB = AggregatorV3Interface(
-            0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE  // BNB/USD
-        );
-        dataFeedETH = AggregatorV3Interface(
-            0x9ef1B8c0E4F7dc8bF5719Ea496883DC6401d5b2e  // ETH/USD
-        );
+        // dataFeedBNB = AggregatorV3Interface(
+        //     0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE  // BNB/USD
+        // );
+        // dataFeedETH = AggregatorV3Interface(
+        //     0x9ef1B8c0E4F7dc8bF5719Ea496883DC6401d5b2e  // ETH/USD
+        // );
 
         NAXY = IERC20(0xf96fF891F0c271a89Dae17346791B1FA524fC681);
         USDT_ADDRESS = 0x55d398326f99059fF775485246999027B3197955;
         WETH_ADDRESS = 0x2170Ed0880ac9A755fd29B2688956BD959F933F8; 
+        BUSDC_ADDRESS = 0x2170Ed0880ac9A755fd29B2688956BD959F933F8; // change this 
         owner = msg.sender;
     }
     
@@ -55,39 +58,42 @@ contract Presale  {
         NAXY.transfer(recipient, NaxyBalance);
     }
 
-    function getChainlinkDataFeedLatestAnswerBNB() public view returns (uint256) {
-        (
-            ,
-            int answer,
-            ,
-            ,
-        ) = dataFeedBNB.latestRoundData();
-        return uint256(answer*1e10);
-    }
+    // function getChainlinkDataFeedLatestAnswerBNB() public view returns (uint256) {
+    //     (
+    //         ,
+    //         int answer,
+    //         ,
+    //         ,
+    //     ) = dataFeedBNB.latestRoundData();
+    //     return uint256(answer*1e10);
+    // }
 
-    function getChainlinkDataFeedLatestAnswerETH() public view returns (uint256) {
-        (
-            ,
-            int answer,
-            ,
-            ,
-        ) = dataFeedETH.latestRoundData();
-        return uint256(answer*1e10);
-    }
+    // function getChainlinkDataFeedLatestAnswerETH() public view returns (uint256) {
+    //     (
+    //         ,
+    //         int answer,
+    //         ,
+    //         ,
+    //     ) = dataFeedETH.latestRoundData();
+    //     return uint256(answer*1e10);
+    // }
 
     function buyToken(uint currency, uint amountApproved) public returns (bool) {
         IERC20 tokenContract;
         uint totalUsdt;
         if(currency == 1){   // USDT
             tokenContract = IERC20(USDT_ADDRESS);
-            amountInUSDT += amountApproved
+            amountInUSDT += amountApproved;
             totalUsdt = amountApproved;
-            
-        } else if(currency == 2) { // ETH
+        } else if(currency == 2) {
+            tokenContract = IERC20(BUSDC_ADDRESS);
+            amountInUSDT += amountApproved;
+            totalUsdt = amountApproved;
+        } else if(currency == 3) { // ETH
             tokenContract = IERC20(WETH_ADDRESS);
-            amountInETH += amountApproved
-            // uint256 ethPriceInUSD = getChainlinkDataFeedLatestAnswer(); 
-            // totalUsdt = (amountApproved * ethPriceInUSD) / 1e18;   
+            amountInETH += amountApproved;
+            // uint256 ethPriceInUSD = getChainlinkDataFeedLatestAnswerETH(); 
+            totalUsdt = (amountApproved * 2635000000000000000000) / 1e18;   
         } else {
             return false;
         }
@@ -95,30 +101,22 @@ contract Presale  {
         uint256 tokensToBuy = totalUsdt / currentPrice;
         require(tokenContract.allowance(msg.sender, address(this)) >= amountApproved, "Insufficient allowance");
         require(tokenContract.balanceOf(msg.sender) >= amountApproved, "Insufficient balance");
-         
-        // require(tokensToBuy <= ((35000000` * 10 ** 18) - tokensSold), "Not enough tokens left");
         bool sent = tokenContract.transferFrom(msg.sender, owner, amountApproved);
         // totalRaised += amountApproved;
         require(sent, "Token transfer failed");
         tokensSold += tokensToBuy * 10 ** 18;   
-        totalTokensBought[msg.sender] += tokensToBuy * 10 ** 18
+        totalTokensBought[msg.sender] += tokensToBuy * 10 ** 18;
         return true;
     }
 
-    function buyTokenWithETH() public payable returns(bool) {
-        require(msg.value > 0, "No ETH sent");
-
-        // uint256 ethPriceInUSD = getChainlinkDataFeedLatestAnswer(); 
-        uint256 amountInUSD = (msg.value * 1000000000000000000000) / 1e18;
-        totalRaised += amountInUSD ;
-        uint256 currentPrice = calculatePrice(); 
+    function buyTokenWithBNB() public payable returns(bool) {
+        require(msg.value > 0, "No BNB sent");
+        // uint256 ethPriceInUSD = getChainlinkDataFeedLatestAnswerBNB(); 
+        uint256 amountInUSD = (msg.value * 520000000000000000000) / 1e18;
         uint256 tokensToBuy = (amountInUSD * 1e18) / currentPrice;
-        require(tokensToBuy <= ((35000000 * 10 ** 18) - tokensSold), "Not enough tokens left");
         tokensSold += tokensToBuy; 
-        totalTokensBought[msg.sender] += tokensToBuy * 10 ** 18
+        totalTokensBought[msg.sender] += tokensToBuy * 10 ** 18;
         return true;
     }
-
-    
 }
 
